b2a3a91265f68cb09527525635f0865fc887b5df
Krupson <80791068+Krupsonus@users.noreply.github.com>
2022-04-19 08:31:21 +0200

    [1][Bartosz] Test pass

---
+++ TDD-Program/TDD-Program/Parser.cs
@@ -0,0 +1,14 @@
+﻿namespace TDD_Program
+{
+    public class Parser
+    {
+        public int max(int a, int b)
+        {
+            if(a < b)
+            {
+                return b;
+            }
+            return a;
+        }
+    }
+}
