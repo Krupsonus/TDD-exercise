288a779323ebd0f1b1ccaa04fa5448274583613e
Krupson <80791068+Krupsonus@users.noreply.github.com>
2022-04-19 08:41:08 +0200

    [2][Kacper] Refactor and test pass

--- TDD-Program/TDD-Program/Parser.cs
+++ TDD-Program/TDD-Program/Parser.cs
@@ -9,14 +9,7 @@
 
         public int min(int a, int b)
         {
-            if(a < b)
-            {
-                return a;
-            }
-            else
-            {
-                return b;
-            }
+            return Math.Min(a, b);
         }
     }
 }
